f0fbbc296f50e1dbd01d672eba95146f475f0bc9
Adrian1426 <adrianhh1426@gmail.com>
2021-01-10 12:53:10 -0600

    libreria Feign, cliente rest

--- src/main/java/com/servicios/app/items/SpringbootServiciosItemsApplication.java
+++ src/main/java/com/servicios/app/items/SpringbootServiciosItemsApplication.java
@@ -2,7 +2,9 @@ package com.servicios.app.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class SpringbootServiciosItemsApplication {
 
