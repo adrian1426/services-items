65391acee03ff752c84b0293ac58798bb5dd0813
Adrian1426 <adrianhh1426@gmail.com>
2021-01-09 18:18:26 -0600

    Controller - consumo de servicio externo por url - forma 1

---
+++ src/main/java/com/servicios/app/items/controllers/ItemController.java
@@ -0,0 +1,29 @@
+package com.servicios.app.items.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.servicios.app.items.models.Item;
+import com.servicios.app.items.models.service.ItemService;
+
+@RestController
+public class ItemController {
+	
+	@Autowired
+	private ItemService itemService;
+	
+	@GetMapping("/items")
+	public List<Item> getItems(){
+		return itemService.findAll();
+	}
+	
+	@GetMapping("/items/{id}/cantidad/{cantidad}")
+	public Item getItemById(@PathVariable Long id,@PathVariable Integer cantidad) {
+		return itemService.findById(id, cantidad);
+	}
+
+}
